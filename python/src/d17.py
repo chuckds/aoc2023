412919f5053afe0c175ed066fed39725adb39d41
Christopher Davidson <chdavids@cisco.com>
2023-12-17 14:21:21 +0000

    d17 tidy

--- python/src/d17.py
+++ python/src/d17.py
@@ -19,10 +19,8 @@ class Direction(enum.Enum):
     EAST = (1, 0)
 
 
-DIR_TO_LEFT = {dir: Direction((-1 * dir.value[1], dir.value[0]))
-               for dir in Direction}
-DIR_TO_RIGHT = {dir: Direction((dir.value[1], -1 * dir.value[0]))
-                for dir in Direction}
+DIR_TO_OPPOSITE = {dir: Direction((dir.value[0] * -1, dir.value[1] * -1))
+                   for dir in Direction}
 
 
 class Coord(NamedTuple):
@@ -33,63 +31,82 @@ class Coord(NamedTuple):
         return Coord(self.col + direction.value[0], self.line + direction.value[1])
 
 
-def in_bounds(loc: Coord, size: int) -> bool:
-    return 0 <= loc[0] < size and 0 <= loc[1] < size
+class CityBlock(NamedTuple):
+    location: Coord
+    heat_loss: int
+    connected: dict[Direction, CityBlock]
+
+    def __hash__(self) -> int:
+        return hash(self.location)
 
 
-def next_dirs_p1(prev_dir: Direction, num_straights: int) -> tuple[Direction, ...]:
-    if num_straights == 3:
-        return (DIR_TO_LEFT[prev_dir], DIR_TO_RIGHT[prev_dir])
-    return (DIR_TO_LEFT[prev_dir], DIR_TO_RIGHT[prev_dir], prev_dir)
+def construct_city(grid: list[list[int]]) -> dict[Coord, CityBlock]:
+    city_blocks: dict[Coord, CityBlock] = {}
+    for line_idx, line in enumerate(grid):
+        for col_idx, heat_loss in enumerate(line):
+            location = Coord(col_idx, line_idx)
+            city_blocks[location] = CityBlock(location, heat_loss, {})
+
+    for location, city_block in city_blocks.items():
+        for in_dir in Direction:
+            new_loc = location.in_direction(in_dir)
+            adj = city_blocks.get(new_loc)
+            if adj:
+                city_block.connected[in_dir] = adj
+
+    return city_blocks
 
 
-def next_dirs_p2(prev_dir: Direction, num_straights: int) -> tuple[Direction, ...]:
-    if num_straights == 10:
-        return (DIR_TO_LEFT[prev_dir], DIR_TO_RIGHT[prev_dir])
-    if num_straights < 4:
-        return (prev_dir,)
-    else:
-        return (DIR_TO_LEFT[prev_dir], DIR_TO_RIGHT[prev_dir], prev_dir)
-
-
-def min_heat_loss(grid: list[list[int]], start: Coord, end: Coord, p2: bool) -> int:
-    n_dir = next_dirs_p2 if p2 else next_dirs_p1
-    to_check = [((start, 0, Direction.SOUTH), 0), ((start, 0, Direction.EAST), 0)]
+def min_heat_loss_cb(start: CityBlock, end: CityBlock, p2: bool) -> int:
+    to_check: list[tuple[int, tuple[CityBlock, int, Direction]]] = [
+        (0, (start, 0, Direction.SOUTH)),
+        (0, (start, 0, Direction.EAST))
+    ]
     visited = set()
-    size = len(grid[0])
     while to_check:
-        walk_state, heat_loss = to_check.pop()
+        heat_loss, walk_state = to_check.pop()
         if walk_state in visited:
             continue
         visited.add(walk_state)
-        coord, num_straights, prev_dir = walk_state
-        if coord == end:
-            return heat_loss
-        dirs = n_dir(prev_dir, num_straights)
-        for new_dir in dirs:
-            new_loc = coord.in_direction(new_dir)
-            if not in_bounds(new_loc, size):
+        block, num_straights, prev_dir = walk_state
+        if block == end:
+            return -1 * heat_loss
+        from_dir = DIR_TO_OPPOSITE[prev_dir]
+        for new_dir, new_block in block.connected.items():
+            if new_dir == from_dir:  # Can't go backwards
                 continue
-            new_num_straights = 1
-            if new_dir == prev_dir:
-                new_num_straights += num_straights
-            walk_state = (new_loc, new_num_straights, new_dir)
+            if new_dir == prev_dir:  # Straight on
+                if num_straights >= (10 if p2 else 3):
+                    continue
+            elif p2 and num_straights <= 3:
+                continue
+            walk_state = (
+                new_block,
+                num_straights + 1 if new_dir == prev_dir else 1,
+                new_dir
+            )
             if walk_state in visited:
                 continue
-            new_heat_loss = grid[new_loc.line][new_loc.col] + heat_loss
-            bisect.insort(to_check, (walk_state, new_heat_loss), key=lambda x: -1 * x[1])
+            bisect.insort(
+                to_check,
+                (heat_loss - new_block.heat_loss, walk_state),
+                key=lambda x: x[0],
+            )
     return 0
 
 
-def p1p2(input_file: Path = utils.real_input()) -> tuple[int | None, int | None]:
+def p1p2(input_file: Path = utils.real_input()) -> tuple[int, ...]:
     grid = [
         [int(char) for char in line]
         for line in input_file.read_text().splitlines()
     ]
 
-    return (
-        min_heat_loss(grid, Coord(0, 0), Coord(len(grid[0]) - 1, len(grid[0]) - 1), False),
-        min_heat_loss(grid, Coord(0, 0), Coord(len(grid[0]) - 1, len(grid[0]) - 1), True)
+    city_blocks = construct_city(grid)
+    return tuple(
+        min_heat_loss_cb(city_blocks[Coord(0, 0)],
+                         city_blocks[Coord(len(grid[0]) - 1, len(grid[0]) - 1)],
+                         is_p2)
+        for is_p2 in (False, True)
     )
 
 
